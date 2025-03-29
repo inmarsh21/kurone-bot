451051a4a4057103f9487ae1c9307aa45290d0bd
inmarsh21 <inmarsh21@yahoo.co.jp>
2025-03-29 16:46:16 +0900

    ポート修正

--- kurone_app.py
+++ kurone_app.py
@@ -557,3 +557,7 @@ def get_today_str():
 
 # def save_result_to_db(user_id, result):
 #     pass  # TODO: 実装予定
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
